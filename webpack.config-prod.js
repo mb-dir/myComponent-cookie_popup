cc5750f548a1013d45ae01e9466b50d6dcc9ce00
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-01-09 17:32:27 +0100

    Create prod mode file
    It is just a copy of dev mode, but without devserver

---
+++ webpack.config-prod.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main[contenthash:5].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        })
+    ],
+};
