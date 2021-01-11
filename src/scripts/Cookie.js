99273d1ce2f03c42c8d8644c978ebbb3743ab032
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-01-11 15:34:45 +0100

    Add new method
    This method is responsible for setting the special key in localstore
    afret the user clicks the accept button. This method also changes the
    style for modal

--- src/scripts/Cookie.js
+++ src/scripts/Cookie.js
@@ -2,6 +2,16 @@ class Cookie{
     constructor(cookieModal, btnAccept){
         this.cookieModal = cookieModal;
         this.btnAccept = btnAccept;
+
+        this.acceptCookie();
+    }
+
+    acceptCookie(){
+        this.btnAccept.addEventListener("click",()=>{
+            this.cookieModal.style.display = "none";
+
+            localStorage.setItem("cookieAccept", 1);
+        });
     }
 }
 
