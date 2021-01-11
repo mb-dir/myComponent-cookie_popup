dd03b9c51e4790aff6f1ed9e8ac84fcc88fcd5bd
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-01-11 15:40:50 +0100

    Create the instance of the Cookie class

--- src/index.js
+++ src/index.js
@@ -7,3 +7,6 @@ import Cookie from "./scripts/Cookie.js";
 //Variables related with DOM
 const modal = document.querySelector("#modal");
 const btnAccept = document.querySelector("#btnAccept");
+
+//Classes
+const cookie = new Cookie(modal, btnAccept);
