b69ed40ad5b79ea14f9f8028179ac1b2562da7de
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-01-11 15:31:56 +0100

    Add variables related with DOM
    I will pass these variables to cookie component, because these elements
    are required

--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ import css from "./styles/style.css";
 //JS imports
 import Cookie from "./scripts/Cookie.js";
 
+//Variables related with DOM
+const modal = document.querySelector("#modal");
+const btnAccept = document.querySelector("#btnAccept");
